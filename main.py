b4b8bad58da1645b9bb74880c71acabf7bb898df
yanglbme <szuyanglb@outlook.com>
2020-11-23 08:42:49 +0800

    feat: use actions toolkit

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import base64
 import hashlib
 import hmac
 import json
-import os
 import re
 import time
 import urllib.parse
+from actions_toolkit.core import get_input
 
 import requests
 import requests.packages.urllib3
+from actions_toolkit import core
 from fake_useragent import UserAgent
 
 requests.packages.urllib3.disable_warnings()
@@ -107,10 +108,9 @@ class Action:
 
 
 if __name__ == '__main__':
-    input_hook = os.environ.get('INPUT_WEBHOOK')
-    input_secret = os.environ.get('INPUT_SECRET')
-    input_count = 8 if not os.environ.get('INPUT_COUNT') \
-        else int(os.environ.get('INPUT_COUNT'))
-
+    input_hook = core.get_input('webhook')
+    input_secret = core.get_input('secret', required=False)
+    count_str = core.get_input('count')
+    input_count = int(count_str) if count_str else 8
     action = Action(input_hook, input_secret, input_count)
     action.run()
